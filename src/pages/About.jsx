e98a952d35e190136a0090c562a9b4ab1c928326
DonielYos-Z <donyoszaner@gmail.com>
2025-09-26 13:02:46 +0300

    Update About.jsx

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -2,6 +2,14 @@ import React from 'react'
 
 export default function About() {
     return (
-        <div>About</div>
+        <div className='container'>
+            <h2>About This Project</h2>
+            <p>
+                This React app searches and displays information about movies. 
+                You can view details such as release date or rating, read a brief description of the plot,
+                and navigate easily between the movie list and detailed views.
+            </p>
+        </div>
     )
 }
+
